65432f47b3c091423348a7b3ad29ba07f6bea6d3
Tyler Lin <lintyl@bc.edu>
2025-09-12 22:30:31 -0400

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Tyler Lin on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
